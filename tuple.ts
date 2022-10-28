23ed6b76acb7e9d1e80e0b052a642c45616aa3e7
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-28 20:43:40 +0900

    tuple

---
+++ tuple.ts
@@ -0,0 +1,8 @@
+let x: [string, number]
+
+x = ["hello", 39] // 순서와 타입이 맞아야 함
+
+// 지금 할당한 x에는 string, number 두 개밖에 없기 때문에 2개만 쓸 수 있음
+
+const person1: [string, number] = ['Mark', 39]
+const [first, second] = person1
