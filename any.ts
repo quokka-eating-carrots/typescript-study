ef77e52aa733d6dba0a3e5aca44715ee4dcfe4bc
quokka-eating-carrots <ovzip7@gmail.com>
2022-10-28 21:47:51 +0900

    any

--- any.ts
+++ any.ts
@@ -11,10 +11,10 @@ let looselyTyped: any = {};
 const d = looselyTyped.a.b.c.d;
 
 function leakingAny(obj: any) {
-  const a = obj.num;
+  const a: number = obj.num;
   const b = a + 1;
   return b;
 }
 
 const c = leakingAny({ num: 0 })
-const d: string = c.indexOf('0');
+c.indexOf('0'); // a에 타입이 지정되어서 indexOf를 사용할 수 없게 됨
